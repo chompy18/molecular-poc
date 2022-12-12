35ee43cb2dfc6938fc399ed4b7064dc4ea02d959
Sefi Ninio <sefininio@Sefis-MacBook-Pro.local>
2022-12-12 15:53:32 +0200

    moleculer init projects

---
+++ counter/services/counter.service.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/**
+ * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+/** @type {ServiceSchema} */
+module.exports = {
+	name: "counter",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		counter: {
+			rest: {
+				method: "GET",
+				path: "/counter"
+			},
+			async handler() {
+				return "the counter is 0";
+			}
+		},
+
+		/** 
+		 * Welcome, a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			rest: "/welcome",
+			params: {
+				name: "string"
+			},
+			/** @param {Context} ctx  */
+			async handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
